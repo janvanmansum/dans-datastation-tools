52672d4d2475229819fdff143f9fbc65cfef88a4
Jan van Mansum <janvanmansum@gmail.com>
2022-08-19 14:50:47 +0200

    More complete jsonData

--- src/datastation/scripts/prestage_files.py
+++ src/datastation/scripts/prestage_files.py
@@ -3,7 +3,7 @@ import os.path
 import sys
 
 from datastation.config import init
-from datastation.dv_storage import prestage_file, prestage_files, ensure_doi_directory_exists
+from datastation.dv_storage import prestage_file, prestage_files, ensure_doi_directory_exists, new_file_json_data_creator
 
 
 def main():
@@ -11,21 +11,22 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Prestages files in the Dataverse storage directory')
-    parser.add_argument('-d', '--doi', dest='doi', help='the dataset DOI')
+    parser.add_argument('-d', '--doi', dest='doi', help='the dataset DOI', required=True)
     parser.add_argument('file',
                         help='file to prestage; if it is a directory, all files in it will be prestaged recursively')
     args = parser.parse_args()
 
-    file_or_dir = args.file
+    file_or_dir = os.path.expanduser(args.file)
     if not os.path.exists(file_or_dir):
         print('File or directory not found: {}'.format(file_or_dir), file=sys.stderr)
         exit(1)
 
-    ensure_doi_directory_exists(config['dataverse']['files_root'], args.doi)
+    files_root = config['dataverse']['files_root']
+    ensure_doi_directory_exists(files_root, args.doi)
     if os.path.isfile(file_or_dir):
-        prestage_file(config['dataverse']['files_root'], args.doi, file_or_dir)
+        prestage_file(files_root, args.doi, file_or_dir, new_file_json_data_creator())
     elif os.path.isdir(file_or_dir):
-        prestage_files(config['dataverse']['files_root'], args.doi, file_or_dir)
+        prestage_files(files_root, args.doi, file_or_dir, new_file_json_data_creator(file_or_dir))
 
 
 if __name__ == '__main__':
