540f119b7bd7bb22bae4c4e75d9434edc979911d
Jan van Mansum <janvanmansum@gmail.com>
2022-10-09 10:32:55 +0200

    Added dv-dataset-destroy command

---
+++ src/datastation/scripts/dataset_destroy.py
@@ -0,0 +1,26 @@
+import argparse
+
+from datastation.config import init
+from datastation.dv_api import destroy_dataset
+
+
+def main():
+    config = init()
+    parser = argparse.ArgumentParser(
+        description='Destroy a published dataset. Requires dataverse.safety_latch = OFF + superadmin permissions in '
+                    'Dataverse. Bulk processing NOT SUPPORTED.')
+    parser.add_argument('pid', dest='pid', help='The PID of the dataset to destroy')
+    args = parser.parse_args()
+
+    safety_latch = config['dataverse']['safety_latch']
+    pid = args.pid
+    server_url = config['dataverse']['server_url']
+    api_token = config['dataverse']['api_token']
+    if safety_latch == "OFF":
+        destroy_dataset(server_url, api_token, pid)
+    else:
+        print("safety_latch is ON. Not performing destroy.")
+
+
+if __name__ == '__main__':
+    main()
