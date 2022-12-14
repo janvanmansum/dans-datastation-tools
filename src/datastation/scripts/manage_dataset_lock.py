128bd79895e5354385de5acd159bd0a77b60a352
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2022-12-14 16:41:32 +0100

    Rename sub-commands to align with dv-banner

--- src/datastation/scripts/manage_dataset_lock.py
+++ src/datastation/scripts/manage_dataset_lock.py
@@ -10,14 +10,14 @@ from datastation.dv_api import get_dataset_locks, delete_dataset_locks_all, dele
 
 
 def manage_dataset_lock_action(server_url, api_token, pid, action, lock_type):
-    if action == "unlock":
+    if action == "remove":
         if lock_type:
             delete_lock_type(server_url, api_token, pid, lock_type)
         else:
             delete_all_locks(server_url, api_token, pid)
-    elif action == "show":
+    elif action == "list":
         show_all_locks(server_url, pid)
-    elif action == "lock":
+    elif action == "add":
         create_lock(server_url, api_token, pid, lock_type)
 
 
@@ -42,7 +42,7 @@ def delete_lock_type(server_url, api_token, pid, lock_type):
     else:
         logging.info("{} - Found locks".format(pid))
         logging.debug(json.dumps(resp_data, indent=2))
-        logging.debug("{} - Try deleting the locks".format(pid))
+        logging.debug("{} - Try removing the lock".format(pid))
         delete_dataset_lock(server_url, api_token, pid, lock_type)
         deleted_lock = True
 
@@ -57,7 +57,7 @@ def delete_all_locks(server_url, api_token, pid):
     else:
         logging.info("{} - Found locks".format(pid))
         logging.debug(json.dumps(resp_data, indent=2))
-        logging.debug("{} - Try deleting the locks".format(pid))
+        logging.debug("{} - Try removing the locks".format(pid))
         delete_dataset_locks_all(server_url, api_token, pid)
         deleted_locks = True
 
@@ -75,16 +75,16 @@ def main():
     lock_types = ["Ingest", "Workflow", "InReview", "DcmUpload", "finalizePublication", "EditInProgress",
                   "FileValidationFailed"]
     parser = argparse.ArgumentParser(description='Manage locks of datasets with the pids in the given input file. '
-                                                 'Locking and unlocking requires superuser privileges.')
+                                                 'Adding and removing locks requires superuser privileges.')
     subparsers = parser.add_subparsers(help='sub-command help', dest='action')
 
-    parser_show = subparsers.add_parser('show', help='show the locks on the datasets')
-    parser_lock = subparsers.add_parser('lock', help="create a lock on the datasets of the given type")
-    parser_lock.add_argument('-t', '--lock-type', dest='lock_type', choices=lock_types,
-                             help='The lock type to be created for the datasets', required=True)
+    parser_list = subparsers.add_parser('list', help='List the locks on the datasets')
+    parser_add = subparsers.add_parser('add', help="Add a lock on the datasets of the given type")
+    parser_add.add_argument('-t', '--lock-type', dest='lock_type', choices=lock_types,
+                             help='The lock type to be added for the datasets', required=True)
 
-    parser_unlock = subparsers.add_parser('unlock', help='delete the given locktype, or all if none is provided')
-    parser_unlock.add_argument('-t', '--lock-type', dest='lock_type', choices=lock_types,
+    parser_remove = subparsers.add_parser('remove', help='Remove the given locktype, or all if no locktype is provided')
+    parser_remove.add_argument('-t', '--lock-type', dest='lock_type', choices=lock_types,
                                help='The lock type to be deleted for the datasets',)
     parser_group_pidargs = parser.add_mutually_exclusive_group()
     parser_group_pidargs.add_argument('-d', '--datasets', dest='pids_file', help='The input file with the dataset pids')
@@ -97,7 +97,7 @@ def main():
     api_token = config['dataverse']['api_token']
 
     chosen_lock_type = None
-    if args.action != 'show':
+    if args.action != 'list':
         chosen_lock_type = args.lock_type
     if args.pid is not None:
         manage_dataset_lock_action(server_url, api_token, args.pid, args.action, chosen_lock_type)
