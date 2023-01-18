0ed23ca7c72eb3b6cb943c8a5bee7fa9a3d48188
Jan van Mansum <janvanmansum@gmail.com>
2023-01-18 13:07:19 +0100

    More command line help

--- src/datastation/scripts/ingest_flow_progress_report.py
+++ src/datastation/scripts/ingest_flow_progress_report.py
@@ -7,7 +7,9 @@ from datastation.config import init
 
 def main():
     config = init()
-    parser = argparse.ArgumentParser(description='Print progress report for a batch')
+    parser = argparse.ArgumentParser(description='Print progress report for a batch. The progress report consists of a timestamp,'
+                                     ' and the numbers of deposits still to do, processed, rejected and failed. If a directory is passed that is'
+                                     ' not in one of the configured ingest areas, an error is returned.')
     parser.add_argument('deposits_batch', metavar='<deposits-batch>', help='Path to the batch of deposits to print the progress report for')
 
     args = parser.parse_args()
