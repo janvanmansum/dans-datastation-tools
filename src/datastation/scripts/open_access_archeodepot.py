fa4f741909f0bbc505156d8ed1469be3fce8362a
jo-pol <jo-pol@users.noreply.github.com>
2023-02-13 10:24:36 +0100

    with mapping

--- src/datastation/scripts/open_access_archeodepot.py
+++ src/datastation/scripts/open_access_archeodepot.py
@@ -1,25 +1,41 @@
 import argparse
 import logging
+import csv
+import re
 
 from datastation.batch_processing import batch_process
 from datastation.config import init
-from datastation.ds_pidsfile import load_pids
 from datastation.dv_api import publish_dataset, get_dataset_metadata, change_access_request, change_file_restrict, \
     replace_dataset_metadata
 
 
-def open_access_archeodepot(dois, mapping, dataverse_config, dry_run, delay):
-    batch_process(dois, lambda doi: update(doi, mapping, dry_run, dataverse_config), delay)
+def open_access_archeodepot(datasets_file, name_to_uri_file, dataverse_config, dry_run, delay):
+    name_to_uri = {}
+    with open(name_to_uri_file, "r") as input_file_handler:
+        csv_reader = csv.DictReader(input_file_handler, delimiter=',')
+        for row in csv_reader:
+            name_to_uri[name_to_key(row["name"])] = row["url"]
+    doi_to_license_name = {}
+    with open(datasets_file, "r") as input_file_handler:
+        csv_reader = csv.DictReader(input_file_handler, delimiter=',')
+        for row in csv_reader:
+            doi_to_license_name[row["DOI"]] = name_to_key(row["LICENSE_NAME"])
+    batch_process(doi_to_license_name.items(),
+                  lambda key_value: update(key_value[0], name_to_uri[key_value[1]], dry_run, dataverse_config),
+                  delay)
 
 
-def update(doi, mapping, dry_run, dataverse_config):
+def name_to_key(name):
+    return re.sub("[- .]", "_", name)
+
+
+def update(doi, uri, dry_run, dataverse_config):
     server_url = dataverse_config['server_url']
     api_token = dataverse_config['api_token']
     resp_data = get_dataset_metadata(server_url, api_token, doi)
     dirty = False
-    if resp_data['license']['name'] != 'CC0 1.0':  # TODO apply mapping
+    if name_to_key(resp_data['license']['uri']) != uri:
         dirty = True
-        uri = "http://creativecommons.org/publicdomain/zero/1.0"
         json_data = '{"http://schema.org/license": "' + uri + '"}'
         logging.info(json_data)
         if not dry_run:
@@ -56,17 +72,17 @@ def mdb_field_value(resp_data, metadata_block, field_name):
 def main():
     config = init()
     parser = argparse.ArgumentParser(description='Change archeodepot dataset to open access')
-    parser.add_argument('-d', '--datasets', dest='pids_file',
-                        help='The input file with the dataset pids')
-    parser.add_argument('-m', '--mapping', dest="mapping",
-                        help="file with license per rights holder")
+    parser.add_argument('-d', '--datasets', dest='datasets',
+                        help='CSV file with: DOI, depositor(ignored), LICENSE_NAME')
+    parser.add_argument('-l', '--licenses', dest='licenses',
+                        help='CSV file with: uri, name')
     parser.add_argument('--delay', default=5.0,
                         help="Delay in seconds (because publish is doing a lot after the async. request is returning)")
     parser.add_argument('--dry-run', dest='dry_run', action='store_true',
                         help="only logs the actions, nothing is executed")
 
     args = parser.parse_args()
-    open_access_archeodepot(load_pids(args.pids_file), args.mapping, config['dataverse'], args.dry_run,
+    open_access_archeodepot(args.datasets, args.licenses, config['dataverse'], args.dry_run,
                             float(args.delay))
 
 
