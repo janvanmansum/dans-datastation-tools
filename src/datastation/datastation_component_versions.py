646f871e31814510c99b33ba2e235203e20f9942
Jan van Mansum <janvanmansum@gmail.com>
2023-11-28 11:19:20 +0100

    print as JSON

--- src/datastation/datastation_component_versions.py
+++ src/datastation/datastation_component_versions.py
@@ -1,3 +1,5 @@
+import argparse
+
 from rich.console import Console
 from rich.table import Table
 
@@ -8,19 +10,30 @@ from datastation.common.version_info import get_rpm_versions, get_dataverse_vers
 
 def main():
     config = init()
+
+    parser = argparse.ArgumentParser(
+        description='Gets the version of all Data Station components in this installation.')
+    parser.add_argument('--json', dest='json', action='store_true', help='Output as JSON')
+    args = parser.parse_args()
+
     components = get_rpm_versions(config['version_info']['dans_rpm_module_prefix'])
     dataverse_version = get_dataverse_version(config['version_info']['dataverse_application_path'])
     dataverse_build_number = get_dataverse_build_number(config['version_info']['dataverse_application_path'])
     components['dataverse'] = f'{dataverse_version} build {dataverse_build_number}'
     payara_version = get_payara_version(config['version_info']['payara_install_path'])
     components['payara'] = payara_version
-    table = Table(title="Data Station Component Versions")
-    table.add_column("Component")
-    table.add_column("Version")
-    for component in components:
-        table.add_row(component, components[component])
-    console = Console()
-    console.print(table)
+
+    if args.json:
+        print(components)
+        return
+    else:
+        table = Table(title="Data Station Component Versions")
+        table.add_column("Component")
+        table.add_column("Version")
+        for component in components:
+            table.add_row(component, components[component])
+        console = Console()
+        console.print(table)
 
 
 if __name__ == '__main__':
