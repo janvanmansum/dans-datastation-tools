739e00786eb693aa5c19971c3c3c403106eedb6f
Jan van Mansum <janvanmansum@gmail.com>
2023-11-28 10:57:36 +0100

    Correct calls in main

--- src/datastation/datastation_component_versions.py
+++ src/datastation/datastation_component_versions.py
@@ -2,15 +2,17 @@ from rich.console import Console
 from rich.table import Table
 
 from datastation.common.config import init
-from datastation.common.version_info import get_rpm_versions, get_dataverse_version
+from datastation.common.version_info import get_rpm_versions, get_dataverse_version, get_dataverse_build_number, \
+    get_payara_version
 
 
 def main():
     config = init()
     components = get_rpm_versions(config['version_info']['dans_rpm_module_prefix'])
     dataverse_version = get_dataverse_version(config['version_info']['dataverse_application_path'])
+    dataverse_build_number = get_dataverse_build_number(config['version_info']['dataverse_application_path'])
     components['dataverse'] = dataverse_version
-    payara_version = get_dataverse_version(config['version_info']['payara_install_path'])
+    payara_version = get_payara_version(config['version_info']['payara_install_path'])
     components['payara'] = payara_version
     table = Table(title="Data Station Component Versions")
     table.add_column("Component")
