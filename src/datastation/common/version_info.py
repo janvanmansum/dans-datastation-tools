2f752be6eb6a442ec7943e3f9594a57045cb286b
Jan van Mansum <janvanmansum@gmail.com>
2023-11-29 10:11:00 +0100

    WIP

--- src/datastation/common/version_info.py
+++ src/datastation/common/version_info.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from re import match
 
@@ -6,7 +7,7 @@ def rpm_qa():
     return os.popen('rpm -qa')
 
 
-evr_pattern = r'(?P<name>.*?)-(?P<version>\d+\.\d+\.\d+)-(?P<release>\d+)'
+evr_pattern = r'(?P<name>.*?)-(?P<version>\d+\.\d+\.\d+)-(?P<release>[^\.]+)\.(?P<arch>.+)'
 payara_version_pattern = r'Thank you for downloading Payara Server (.*).'
 
 
@@ -15,10 +16,15 @@ def get_rpm_versions(prefix):
     rpm_versions = {}
     for line in rpm_qa():
         if line.startswith(prefix):
+            logging.debug(f'Found RPM matching prefix {prefix}: {line}')
             evr = match(evr_pattern, line)
             version = evr.group('version')
             module = evr.group('name')
-            rpm_versions[module] = version
+            release = evr.group('release')
+            # Adding the release part of the EVR is not very informative for DANS modules,
+            # because it is always 1 for releases, except for SNAPSHOTs, in which case it is
+            # important to know the exact SNAPSHOT version.
+            rpm_versions[module] = f'{version}-{release}' if release != '1' else f'{version}'
 
     return rpm_versions
 
@@ -40,8 +46,6 @@ def get_dataverse_build_number(dataverse_application_path):
 
 def get_payara_version(payara_application_path):
     with open(os.path.join(payara_application_path, 'README.txt'), 'r') as f:
-        # Find first line that matches pattern
         line = next((line for line in f if match(payara_version_pattern, line)), None)
-        # get subgroup 1 of the match
         payara_version = match(payara_version_pattern, line).group(1)
     return payara_version
