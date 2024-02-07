bf1aafed4dc4aedf9583ce00ed4c6830486af051
paulboon <paul.boon@dans.knaw.nl>
2024-02-07 15:15:26 +0100

    Permission collect describes the output in the help text

--- src/datastation/dv_dataverse_root_collect_permission_overview.py
+++ src/datastation/dv_dataverse_root_collect_permission_overview.py
@@ -1,5 +1,5 @@
 import argparse
-
+from argparse_formatter import FlexiFormatter
 
 from datastation.common.config import init
 from datastation.common.utils import add_dry_run_arg
@@ -10,9 +10,26 @@ from datastation.dataverse.permissions_collect import PermissionsCollect
 def main():
     config = init()
 
-    parser = argparse.ArgumentParser(description='Collect the permissions overview for the dataverses')
+    output_explanation = '''
+    The output has the following information:
+    
+      * depth: The depth in the tree of the . The top-level ('root') has depth 0.
+      * parentalias: The alias of the parent dataverse
+      * alias: The alias of the dataverse
+      * name: The name of the dataverse
+      * id: The id of the dataverse, sometimes used in places where the alias is not used
+      * vpath: The virtual path of the collection, i.e. the path from the root to the dataverse
+      * groups: A comma-separated list of the explicit groups under the dataverse. 
+        For each group there is the 'identifier' with the number of 'containedRoleAssignees' in braces appended. 
+      * roles:  A comma-separated list of the roles defined in the dataverse. 
+        For each role there is the 'alias' with the number of 'permissions' in braces appended.
+      * assignments: A comma-separated list of the assignments of roles on the dataverse. 
+        For each assignment there is the 'assignee' with the '_roleAlias' in braces appended.
+    '''
+    parser = argparse.ArgumentParser(description='Collect the permissions overview for the dataverses (collections) in a Dataverse installation.',
+                                     epilog=output_explanation, formatter_class=FlexiFormatter)
     parser.add_argument('-o', '--output-file', dest='output_file', default='-',
-                        help='the file to write the output to or - for stdout')
+                        help='The file to write the output to or - for stdout')
     parser.add_argument('-f', '--format', dest='format',
                         help='Output format, one of: csv, json (default: json)')
 
